edf6ff7c3c7ebfa35558cbf815279c821068a7ca
Joel Borja <joel.borjacastrejon.620@my.csun.edu>
2021-12-02 13:14:47 -0800

    Fetches Movies on homepage

--- src/components/Card/MovieCard.jsx
+++ src/components/Card/MovieCard.jsx
@@ -3,21 +3,24 @@ import { Card, CardContent, Typography, CardActions, CardMedia, Button } from '@
 import { Favorite } from '@mui/icons-material';
 
 
-export default function MovieCard({title, img, description}) {
+export default function MovieCard({title, img, year,rating}) {
     return (
       <Card sx={{ maxWidth: 345 }}>
         <CardMedia
           component="img"
           alt="green iguana"
           height="140"
-          image="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2059&q=80"
+          image = {img}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Test, Real title: {title}
+           {title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Temporary text. Real description: {description}
+          <Typography variant="h6" color="text.secondary">
+            {year}
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Rating: {rating}
           </Typography>
         </CardContent>
         <CardActions>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,20 +1,76 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import styles from '../styles/home.module.css';
-import { Container, Typography} from '@mui/material';
+import { Container, Typography, Grid} from '@mui/material';
+import MovieCard from '../Card/MovieCard';
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button} from '@mui/material'
+import { MovieSharp } from '@mui/icons-material';
 
 export default function Home() {
+
+    const [movies, setMovies] = useState([])
+    const [shows, setShows] = useState([])
+    useEffect(() =>{
+        getMovie()
+    },[])
+    const getMovie = async () => {
+        const response = await fetch('https://imdb-api.com/en/API/Top250Movies/k_5sh8gslw')
+        const data = await response.json()
+        console.log(data.items);
+        let movies = []
+        data.items.map((e, i ) => {
+            if (i < 12)
+                movies.push(e)
+            });
+        setMovies(movies)
+        // setShows(data.data.tv_shows)
+    }
     return (
-        // this is basic html with react
-
-        // <div className={styles.wrapper}>
-        //     Home page
-        // </div>
-
-        // this is Material UI, just an example
         <Container className={styles.container} maxWidth="lg">
-            <Typography variant="h2" component="h1" gutterBottom>
-                Home page
+          <Typography variant = "h3" component = "h2" mt = {4} mb = {4}> 
+            New Releases
             </Typography>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    <Grid item xs={2} sm={4} md={4} >
+                        <MovieCard />
+                    </Grid>
+                    <Grid item xs={2} sm={4} md={4} >
+                        <MovieCard />
+                    </Grid>
+                    <Grid item xs={2} sm={4} md={4} >
+                        <MovieCard />
+                    </Grid>
+                </Grid>
+            <Typography variant = "h3" component = "h2" mt = {4} mb = {4}> 
+                Top 12 Movies
+                </Typography>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>                    
+                    { 
+                     movies.map((item,i) => 
+                     <Grid item xs={2} sm={4} md={4}> 
+                        { i<12 ?
+                        
+                            < MovieCard title = {item.title} img = {item.image} year = {item.year} rating = {item.imDbRating}/>                      
+                        
+                       :null }
+                    </Grid>)
+                     }                    
+                    
+                </Grid>
+                <Typography variant = "h3" component = "h2" mt = {4} mb = {4}> 
+            Top 12 TV Shows
+            </Typography>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    <Grid item xs={2} sm={4} md={4} >
+                        <MovieCard />
+                    </Grid>
+                    <Grid item xs={2} sm={4} md={4} >
+                        <MovieCard />
+                    </Grid>
+                    <Grid item xs={2} sm={4} md={4} >
+                        <MovieCard />
+                    </Grid>
+                </Grid>
         </Container>
     )
 }
